fdbc54a2986d2fcbe6bbffa3f40de61495608bc6
Ivan <totmeshkin@gmail.com>
2024-03-09 22:11:04 +0300

    okey?

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Scope("prototype")
+    @Bean(name="cat")
+    public Cat getName(){
+        Cat cat = new Cat();
+        cat.setName("Cat");
+        return cat;
+    }
+
 }
